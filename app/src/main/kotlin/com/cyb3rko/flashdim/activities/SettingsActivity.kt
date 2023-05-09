e40e579dc15df2327a20c0d13aaa11e49f37c2bb
Niko Diamadis <niko@cyb3rko.de>
2023-05-09 22:40:52 +0200

    rfct: Tidy up SettingsActivity

--- app/src/main/kotlin/com/cyb3rko/flashdim/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/cyb3rko/flashdim/activities/SettingsActivity.kt
@@ -23,17 +23,18 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.os.VibratorManager
 import android.view.Gravity
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.cyb3rko.flashdim.R
+import com.cyb3rko.flashdim.databinding.ActivitySettingsBinding
 import com.cyb3rko.flashdim.utils.Safe
 import com.cyb3rko.flashdim.utils.Vibrator
-import com.cyb3rko.flashdim.databinding.ActivitySettingsBinding
+import com.google.android.material.R as MaterialR
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.slider.Slider
 
@@ -60,28 +61,28 @@ internal class SettingsActivity : AppCompatActivity(), OnSharedPreferenceChangeL
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         when (key) {
             Safe.MORSE_VIBRATION, Safe.BUTTON_VIBRATION -> {
-                MaterialAlertDialogBuilder(
-                    this,
-                    com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
-                )
-                    .setIcon(ResourcesCompat.getDrawable(
-                        resources,
-                        android.R.drawable.stat_notify_sync,
-                        theme
-                    ))
-                    .setTitle(getString(R.string.dialog_restart_title))
-                    .setMessage(getString(R.string.dialog_restart_message))
-                    .setPositiveButton(getString(R.string.dialog_restart_positive_button)) { _, _ ->
-                        val intent = packageManager.getLaunchIntentForPackage(packageName)
-                        val componentName = intent!!.component
-                        val mainIntent = Intent.makeRestartActivityTask(componentName)
-                        startActivity(mainIntent)
-                        Runtime.getRuntime().exit(0)
-                    }
-                    .setNegativeButton(getString(R.string.dialog_restart_negative_button), null)
-                    .show()
+                showRestartDialog()
             }
         }
+    }
+
+    private fun showRestartDialog() {
+        MaterialAlertDialogBuilder(
+            this,
+            MaterialR.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
+        )
+            .setIcon(android.R.drawable.stat_notify_sync)
+            .setTitle(getString(R.string.dialog_restart_title))
+            .setMessage(getString(R.string.dialog_restart_message))
+            .setPositiveButton(getString(R.string.dialog_restart_positive_button)) { _, _ ->
+                val intent = packageManager.getLaunchIntentForPackage(packageName)
+                val componentName = intent!!.component
+                val mainIntent = Intent.makeRestartActivityTask(componentName)
+                startActivity(mainIntent)
+                Runtime.getRuntime().exit(0)
+            }
+            .setNegativeButton(getString(R.string.dialog_restart_negative_button), null)
+            .show()
     }
 
     internal class SettingsFragment : PreferenceFragmentCompat() {
@@ -138,33 +139,33 @@ internal class SettingsActivity : AppCompatActivity(), OnSharedPreferenceChangeL
                         }
                     }
                     content.addView(slider)
+                    showInitialLevelDialog(content) {
+                        val value = slider.value.toInt()
+                        Safe.writeInt(myContext, Safe.INITIAL_LEVEL, value)
 
-                    MaterialAlertDialogBuilder(
-                        requireContext(),
-                        com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
-                    )
-                        .setIcon(ResourcesCompat.getDrawable(
-                            resources,
-                            R.drawable._ic_level,
-                            requireContext().theme
-                        ))
-                        .setTitle(getString(R.string.preference_item_initial_level_dialog_title))
-                        .setView(content)
-                        .setPositiveButton(android.R.string.ok) { _, _ ->
-                            val value = slider.value.toInt()
-                            Safe.writeInt(myContext, Safe.INITIAL_LEVEL, value)
-
-                            val summary = getString(R.string.preference_item_initial_level_summary)
-                            preference.summary = "$summary: $value"
-                        }
-                        .setNegativeButton(getString(
-                            R.string.preference_item_initial_level_dialog_negative_button
-                        ), null)
-                        .show()
-
+                        val summary = getString(R.string.preference_item_initial_level_summary)
+                        preference.summary = "$summary: $value"
+                    }
                     true
                 }
             }
+        }
+
+        private fun showInitialLevelDialog(content: View, onSave: () -> Unit) {
+            MaterialAlertDialogBuilder(
+                requireContext(),
+                MaterialR.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
+            )
+                .setIcon(R.drawable._ic_level)
+                .setTitle(getString(R.string.preference_item_initial_level_dialog_title))
+                .setView(content)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    onSave()
+                }
+                .setNegativeButton(getString(
+                    R.string.preference_item_initial_level_dialog_negative_button
+                ), null)
+                .show()
         }
     }
 }
