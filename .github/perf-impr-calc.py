e00e51b96f3d2128b57daec60227b3c08b763907
Niko Diamadis <niko@cyb3rko.de>
2024-10-18 22:56:56 -0400

    ci: Adapt baseline improvement calc

--- .github/perf-impr-calc.py
+++ .github/perf-impr-calc.py
@@ -1,35 +1,15 @@
 import csv
 import os
 
-
-def get_file_line_count():
-    if os.name == "posix":
-        return int(os.popen("wc -l < performance-data.csv").read()[:-1])
-    elif os.name == "nt":
-        return int(os.popen("type performance-data.csv | find /v /c \"\"").read()[:-1]) - 1
-    else:
-        raise Exception("Who are you?")
-
-
 print(f"Reading CSV file in {os.getcwd()}")
 with open("performance-data.csv", "r") as file:
     reader = csv.reader(file, delimiter=',')
-    row_count = get_file_line_count()
-    print(f"Found {row_count} entries")
-    if row_count < 1:
-        raise Exception("Empty CSV file")
     next(reader)
-    coefficient = 0
-    for row in reader:
-        print("Entry:", row)
-        naked = int(row[1])
-        baseline = int(row[2])
-        temp_coefficient = (naked - baseline) / naked
-        print("Coefficient: ", temp_coefficient)
-        coefficient += temp_coefficient
+    base_value = next(reader)[1]
+    new_value = next(reader)[1]
+    percent = ((base_value - new_value) / base_value) * 100
 
-average_percentage = round(coefficient * 100 / row_count, 2)
-print("Average performance enhancement:", average_percentage)
+print("Average performance enhancement:", percent)
 # changelog = os.getenv("CHANGELOG")
 # # changelog = open("changelog.txt", "r", encoding="utf-8").read()
 # marker = "<<marker1>>"
